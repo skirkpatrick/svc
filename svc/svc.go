a4cf9cb221d4b53c91c92b5a6e45295bb2abd1bd
Scott Kirkpatrick <skirkpatrick4@mail.csuchico.edu>
2013-03-02 14:30:43 -0800

    Add documentation to main

--- svc/svc.go
+++ svc/svc.go
@@ -1,7 +1,25 @@
+/*
+    Simple Version Control
+
+    SVC is meant to be an introductory tool for users new to version control.
+
+    The main goal of SVC is simplicity. To achieve this, the ideology of
+    "convention over configuration" is used. SVC will make assumptions with
+    its commands to minimize the use of argument flags.
+
+    Though SVC will not be as powerful or as customizable as other source
+    control tools such as Git or SVN, it should be very easy to learn and use.
+
+    Since SVC is intended for newer programmers, collaboration tools are not
+    a priority. Instead, SVC will focus on getting users used to the idea of
+    commiting, branching, and using common source control tools for solo
+    projects.
+*/
 package main
 
 import (
 )
 
 func main() {
+    
 }
