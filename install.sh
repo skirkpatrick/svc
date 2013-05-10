1d5c98d27466dae2cf5fbc891b72fff2ee800b3e
Abdullah Talakey <abdullah.talakey@gmail.com>
2013-05-10 00:03:48 -0700

    Mac OS X installation script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+
+
+which gx > /dev/null
+OUT=$?
+if [ $OUT -eq 0 ];then
+   echo "User account found!"
+else
+   echo "User account does not exists in /etc/passwd file!"
+fi
+
+echo "Welcomt to SVC version control system installer."
+echo "This script is going to install and set up SVC version control system."
+
+which go > /dev/null
+if [ $? -ne 0 ]; then
+  echo "GO is not installed on your machine. to install SVC version control " \
+       "system GO must be installed: GO is an open source programming environment " \
+       "that makes it easy to build simple, reliable, and efficient software."
+
+  answer=""
+  while [ "$answer" != "Y" -a "$answer" != "N" ]; do
+    echo "Do you want to install GO? (Y/N)"
+    read answer
+    answer=`echo $answer | tr yn YN`
+  done
+
+  if [ "$answer" == "Y" ]; then
+    echo "Installing GO"
+    # git clone https://github.com/skirkpatrick/GetGo.git
+    # GetGo/getgo.sh
+    # rm -rf GetGo
+    echo "Done Installing GO"
+  else
+    echo "Error: Cannot install SVC without installing GO"
+    exit 1
+  fi
+else
+  echo "GO is installed"
+fi
+
+echo "Installing SVC"
+# go get github.com/skirkpatrick/svc
+# go install
+
+echo "Done Installing SVC"
+echo "For more information about SVC version control system type:"
+echo "svc help"
